2bfd74a5716a521b1e8898958ed942230e1aed75
brunobuzzo <30813663+brunobuzzo@users.noreply.github.com>
2017-11-13 15:00:40 -0200

    Add files via upload

---
+++ django_get_started/views.py
@@ -0,0 +1,65 @@
+"""
+Definition of views.
+"""
+
+from django.shortcuts import render
+from django.http import HttpRequest
+from django.template import RequestContext
+from app.models import Curso
+from datetime import datetime
+
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Home Page',
+            'year':datetime.now().year,
+        })
+    )
+	
+
+def contato(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contato.html',
+        context_instance = RequestContext(request,
+        {
+            'year':datetime.now().year,
+        })
+    )
+
+def about(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/about.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'About',
+            'message':'Gerenciador de vestibulares',
+            'year':datetime.now().year,
+        })
+    )
+
+def cadastro_cursos(request):
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/cadastro_cursos.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Cadastro de cursos',
+#            'cursos': ['ADS' , 'SI', 'CC'],
+            'cursos': Curso.objects.all(),
+            'year':datetime.now().year,
+        })
+    )
+
